f082d85b3d01c95ac1981c0521e11667605b9252
ryotay <ryotayamanaka@gmail.com>
2014-09-10 20:03:15 +0900

    commit

--- xslt.js
+++
@@ -1,6 +0,0 @@
-xslt = require('node_xslt')
-var args = process.argv;
-stylesheet = xslt.readXsltFile('./sbml2rdf-0.0.2.xsl');
-document = xslt.readXmlFile('./sbml_sample01.xml');
-transformedString = xslt.transform(stylesheet, document, []);
-console.log(transformedString);
--- xslt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cat header.ttl
-node xslt.js $1 $2 | /usr/bin/sed -e "s/\//-/g"
